b8b6ae2928acb7272a34732c910f60091b19ce04
Dilshod <85446738+dilshodturabov@users.noreply.github.com>
2023-04-19 07:11:23 +0500

    Add files via upload

---
+++ function() arguments.js
@@ -0,0 +1,15 @@
+function sum(a,b){
+  console.log(a+b);
+}
+sum(1,2);
+sum(1,3);
+sum(4,6);
+sum(7,8);
+
+function greeting(name){  
+console.log("Hush kelibsiz " + name + "!");
+}
+
+greeting("Dilshod");
+greeting("Lazizbek");
+greeting("Odiljon");
